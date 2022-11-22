8dbc99d94b18ff110d82d45ebf82a5a44cb8567c
Sayak Mondal <sayakmondal2001@gmail.com>
2022-11-22 11:55:02 +0530

    signin and login page created and can be navigated

--- app/src/main/java/com/example/librarymanagementsystem/login.java
+++ app/src/main/java/com/example/librarymanagementsystem/login.java
@@ -1,5 +1,34 @@
 package com.example.librarymanagementsystem;
 
-public class login {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class login extends AppCompatActivity {
+
+    TextView signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        signup = (TextView) findViewById(R.id.gotosignup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                signup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                startActivity(new Intent(getApplicationContext(), signup.class));
+                    }
+                });
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/librarymanagementsystem/signup.java
+++ app/src/main/java/com/example/librarymanagementsystem/signup.java
@@ -1,25 +1,26 @@
 package com.example.librarymanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView sample;
+public class signup extends AppCompatActivity {
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        sample = (TextView) findViewById(R.id.deeznuts);
+        setContentView(R.layout.signup);
 
-        sample.setOnClickListener(new View.OnClickListener() {
+        login = (TextView) findViewById(R.id.gotologin);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-        startActivity(new Intent(getApplicationContext(), signup.class));
+        startActivity(new Intent(getApplicationContext(),login.class));
             }
         });
     }
